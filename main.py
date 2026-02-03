d37479acdbb4d97259408bbcf6d1803a53520a9e
Kiril Halekou <kirillhalenko1210@gmail.com>
2026-02-03 14:23:58 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,7 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/valentine.html")
+def valentine():
+    return render_template("valentine.html")
 
+@app.route("/404.html")
+def error_404():
+    return render_template("404.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     print(app.url_map)
